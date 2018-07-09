6506a8f05b5b445926cdb11274e9d3ed2089fbe4
Yurii Soldak <ysoldak@gmail.com>
2018-07-09 20:48:28 +0200

    simulate steps/mm error

--- simulator/delta_printer.py
+++ simulator/delta_printer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 
 import math
 from numpy import sqrt, dot, cross, array, zeros
@@ -9,16 +10,17 @@ from numpy.linalg import norm
 
 class DeltaPrinter:
     
-    def __init__(self, l, r, t = 90.0):
+    def __init__(self, l, r, t = 90.0, e = 0):
         self.l = l
         self.r = r
         self.t = math.radians(t) # tower tilt, t<90 means towers tilt outwards
+        self.e = e/100
         # self.h = math.sqrt(self.l*self.l - self.r*self.r)
         self.tower_angles = [210., 330., 90.]
         self.tower_coords = [(
             math.cos(math.radians(angle)) * self.r,
             math.sin(math.radians(angle)) * self.r) for angle in self.tower_angles]
-        self.tower_positions = [0,0,0] # these positions are wrong, home() initialises them
+        self.tower_positions = [None,None,None]
         self.home()
         self.tower_home_positions = [p for p in self.tower_positions]
 
@@ -51,9 +53,11 @@ class DeltaPrinter:
             # tx = self.tower_coords[i][0]
             # ty = self.tower_coords[i][1]
             # new_pos = math.sqrt(self.l*self.l - (tx-x)*(tx-x) - (ty-y)*(ty-y)) + z
-
-            self.tower_positions[i] = new_pos
-
+            if self.tower_positions[i] is not None:
+                pos_delta = new_pos - self.tower_positions[i]
+                self.tower_positions[i] += pos_delta*(1 + self.e)
+            else:
+                self.tower_positions[i] = new_pos
     # Trilateration, https://stackoverflow.com/a/18654302
     # Basically calculate intersection of 3 spheres, simplified version of code above
     def position(self):
@@ -101,18 +105,39 @@ def error(correct, wrong, x, y, z = 0):
     return correct.position()
 
 # args
-# 1 : L (diagonal rod length)
-# 2 : R (delta radius)
-# 3 : T (towers tilt, in degrees; tilt < 90deg means towers tilted outwards, i.e. R increases with height)
-# 4 : X (x coordinate of nozzle to move to)
-# 5 : Y (y coordinate of nozzle to move to)
+# 1 : L (Diagonal rod length)
+# 2 : R (Delta radius)
+# 3 : T (Towers lean, in degrees; lean < 90deg means towers tilted outwards, i.e. R increases with height)
+# 4 : E (Carriage movement error in %, to simpulate effect of wrong steps/mm configuration)
+# 5 : C (Coordinates to move the nozzle to, semicolon separated triplets of 3d coords)
+
+# Example usage:
+#   ./delta_printer.py -l 120 -r 62 -t 89 -e -1 -c '0,10,0;0,-10,5'
 if __name__ == '__main__':
-    #print(sys.argv)
-    printer = DeltaPrinter(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
-    printer.move(float(sys.argv[4]), float(sys.argv[5]))
-    pos = printer.position()
-    print("Nozzle: {0:.3f}, {1:.3f}, {2:.3f}".format(pos[0], pos[1], pos[2]))
-    towers = printer.tower_positions
-    print("Towers: {0:.3f}, {1:.3f}, {2:.3f}".format(towers[0],towers[1],towers[2]))
-    deltas = printer.tower_deltas()
-    print("Deltas: {0:.3f}, {1:.3f}, {2:.3f}".format(deltas[0],deltas[1],deltas[2]))
+
+    l_value = 120.8
+    r_value = 61.7
+    t_value = 90.0
+    e_value = 0.0
+
+    parser = argparse.ArgumentParser(description='Delta errors simulation')
+    parser.add_argument('-l','--l-value',type=float,default=l_value,help='Correct l-value, in mm')
+    parser.add_argument('-r','--r-value',type=float,default=r_value,help='Correct r-value, in mm')
+    parser.add_argument('-t','--t-value',type=float,default=t_value,help='Tower lean, in deg, t<90 means towers leans outwards')
+    parser.add_argument('-e','--e-value',type=float,default=e_value,help='Carriages error, in %')
+    parser.add_argument('-c','--c-value',type=str,default='0,0',help='Coords to move, can specify several, semicolon separated, in mm')
+    args = parser.parse_args()
+
+    printer = DeltaPrinter(args.l_value, args.r_value, args.t_value, args.e_value)
+
+    coords = [x.split(",") for x in args.c_value.split(";")]
+    for coord in coords:
+        coord = [float(coord[0]), float(coord[1]), float(coord[2]) if len(coord) == 3 else 0.0]
+        printer.move(coord[0], coord[1], coord[2])
+        pos = printer.position()
+        print("Nozzle: {0:.3f}, {1:.3f}, {2:.3f}".format(pos[0], pos[1], pos[2]))
+        towers = printer.tower_positions
+        print("Towers: {0:.3f}, {1:.3f}, {2:.3f}".format(towers[0],towers[1],towers[2]))
+        deltas = printer.tower_deltas()
+        print("Deltas: {0:.3f}, {1:.3f}, {2:.3f}".format(deltas[0],deltas[1],deltas[2]))
+        print("")
